8668ec103079ba69d8c629c5af5aab5586dd388f
Ralph Matta <matta.r@etoncollege.org.uk>
2024-06-04 10:03:24 +0100

    Initial Commit

---
+++ BreweryAPI/BreweryAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreweryAPIApp.swift
+//  BreweryAPI
+//
+//  Created by Matta, Ralph (PEPW) on 04/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct BreweryAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
