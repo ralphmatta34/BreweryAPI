6823c412a68f51c6788caad57ca00923fd11102a
Ralph Matta <matta.r@etoncollege.org.uk>
2024-06-07 15:51:28 +0100

    More work done.

--- BreweryAPI/ContentView.swift
+++ BreweryAPI/ContentView.swift
@@ -9,30 +9,32 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var search: String = String()
-    @State private var results: [AutocompleteResult] = []
-    @ObservedObject var apiManager = APIManager.shared
+    @State private var search: String = ""
+    @State private var results: [ApiResult] = []
+    
+    @ObservedObject var apiManager = ApiManager.shared
     
     var body: some View {
+        
         NavigationStack {
-            TextField("Enter a search item", text: $search)
+            TextField("Enter search item", text: $search)
                 .onChange(of: search) { oldValue, newValue in
-                    print("New value is \(newValue)")
+                    print("New value is: \(newValue)")
                     apiManager.searchFor(query: newValue) { results in
                         self.results = results
                     }
                 }
-            Text("API Results: ")
+            
+            Text("API Results:")
             
             List {
                 ForEach(results, id:\.id) { result in
-                    NavigationLink(result.name) {
-                        BreweryDetailsView(breweryId: self.id result.getAllProperties(id: result.id, completion: {
-                                return
-                        }))
+                    NavigationLink("\(result.name)") {
+                        BreweryDetailView(breweryId: result.id)
                     }
                 }
             }
+            .navigationTitle("Brewery API")
         }
     }
 }
